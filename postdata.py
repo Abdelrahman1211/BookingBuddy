1615ce2ccb7d1ccd47a5f1e938b1f86230bb814a
Abdelrahman1211 <79929250+Abdelrahman1211@users.noreply.github.com>
2024-05-07 23:36:28 +0300

    Add files via upload

---
+++ postdata.py
@@ -0,0 +1,48 @@
+reviews = [
+    {
+        'flight_number': 'AB123',
+        'airline_name': 'EGYPTAIR',
+        'rating': 4.5,
+        'review_content': 'Great flight experience.',
+    },
+    {
+        'flight_number': 'CD456',
+        'airline_name': 'FLYNAS',
+        'rating': 3.8,
+        'review_content': 'Average flight. Could be better.',
+    },
+    {
+        'flight_number': 'EF789',
+        'airline_name': 'ETIHAD AIRWAYS',
+        'rating': 4.5,
+        'review_content': 'Exceeded expectations!',
+    },
+    {
+        'flight_number': 'GH012',
+        'airline_name': 'QATAR AIRWAYS',
+        'rating': 4.0,
+        'review_content': 'Comfortable flight. Friendly staff.',
+    },
+    {
+        'flight_number': 'IJ345',
+        'airline_name': 'FLY EMIRATES',
+        'rating': 5.0,
+        'review_content': 'Outstanding flight.',
+    },
+    {
+        'flight_number': 'MS3455',
+        'airline_name': 'SAUDIA',
+        'rating': 4.9,
+        'review_content': 'Highly recommended!',
+    },
+]
+    # Add more reviews as needed
+posts = [
+        {'id': 1, 'title': 'Post 1', 'content': 'This is the content of Post 1.', 'author': 'John Doe', 'date_posted': '2022-01-01'},
+        {'id': 2, 'title': 'Post 2', 'content': 'This is the content of Post 2.', 'author': 'Jane Smith', 'date_posted': '2022-02-02'},
+        {'id': 3, 'title': 'Post 3', 'content': 'This is the content of Post 3.', 'author': 'Alex Johnson', 'date_posted': '2022-03-03'},
+        {'id': 4, 'title': 'Post 4', 'content': 'This is the content of Post 4.', 'author': 'Sarah Thompson', 'date_posted': '2022-04-04'},
+        {'id': 5, 'title': 'Post 5', 'content': 'This is the content of Post 5.', 'author': 'Michael Brown', 'date_posted': '2022-05-05'},
+        {'id': 6, 'title': 'Post 6', 'content': 'This is the content of Post 6.', 'author': 'Emily Wilson', 'date_posted': '2022-06-06'},
+    # Add more posts as needed
+]
